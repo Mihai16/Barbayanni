8608d371bb7f16b790dcda3aaba848843c4763c9
Adrian Cucos <dielorg@gmail.com>
2019-03-05 16:00:02 +0100

    Loading scripts

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # 2ID70-m1
 Milestone 1 project for Data Intensive Systems
 ### Team Members:
-Adrian Cucos
-Asaf Kedem
-Mihail Tifrea
++ Adrian Cucos
++ Asaf Kedem
++ Mihail Tifrea
--- loadData.sql
+++ loadData.sql
@@ -8,6 +8,6 @@
 -- Don't forget to analyze at the end. It can make a difference in query performance.
 -- Example:
 
-COPY myPhonebook(id, name, address,phoneNumber) FROM '/mnt/ramdisk/tables/myphonebook.table' DELIMITER ',' CSV HEADER;
+COPY degrees FROM '/mnt/ramdisk/tables/Degrees.table' DELIMITER ',' CSV HEADER;  COPY students FROM '/mnt/ramdisk/tables/Students.table' DELIMITER ',' CSV HEADER;  COPY teachers FROM '/mnt/ramdisk/tables/Teachers.table' DELIMITER ',' CSV HEADER;  COPY courses FROM '/mnt/ramdisk/tables/Courses.table' DELIMITER ',' CSV HEADER;  COPY course_offers FROM '/mnt/ramdisk/tables/CourseOffers.table' DELIMITER ',' CSV HEADER;  COPY student_registrations FROM '/mnt/ramdisk/tables/StudentRegistrationsToDegrees.table' DELIMITER ',' CSV HEADER;  COPY teacher_assignments FROM '/mnt/ramdisk/tables/TeacherAssignmentsToCourses.table' DELIMITER ',' CSV HEADER;  COPY student_assistants FROM '/mnt/ramdisk/tables/StudentAssistants.table' DELIMITER ',' CSV HEADER;  COPY course_registrations FROM '/mnt/ramdisk/tables/CourseRegistrations.table' DELIMITER ',' CSV HEADER NULL AS 'null';
 
-ANALYZE VERBOSE
+ANALYZE VERBOSE;
