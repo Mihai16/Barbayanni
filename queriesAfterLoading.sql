5aeed9d2609508410543f7191514da028b41a060
Adrian Cucos <dielorg@gmail.com>
2019-03-11 20:19:32 +0100

    kill me

--- queriesAfterLoading.sql
+++ queriesAfterLoading.sql
@@ -1,3 +1,3 @@
-SELECT  co.,c.name, c.description, d.did, c.ects, d.dept,d.description, d.totalECTS, t.* FROM course_offers co, courses c,degrees d ,teacher_assignments ta, teachers t WHERE co.offerID = 1 AND co.cID = c.cID AND c.dID = d.dID AND (ta.offerID = co.offerID and ta.tID = t.tID );
+SELECT  co.* ,c.name, c.description, d.did, c.ects, d.dept,d.description, d.totalECTS, t.* FROM course_offers co, courses c,degrees d ,teacher_assignments ta, teachers t WHERE co.offerID = 1 AND co.cID = c.cID AND c.dID = d.dID AND (ta.offerID = co.offerID and ta.tID = t.tID );
 SELECT co.*, st.*, d.* FROM student_registrations sr, course_offers co, students st, degrees d, student_assistants sa WHERE sr.rID=3 AND sa.rID=sr.rID AND sa.offerID=co.offerID AND sr.sID=st.sID AND d.dID=sr.dID;
 SELECT AVG(grade) FROM course_registrations cr WHERE cr.rID = 3;
