04df2b40f9933678785e1ed6c268fb5c4826aad9
Adrian Cucos <dielorg@gmail.com>
2019-03-11 16:56:12 +0100

    fixed typo

--- queriesAfterLoading.sql
+++ queriesAfterLoading.sql
@@ -1,4 +1,4 @@
 -- here you will write the three queries requested in Question 1. Each query should be in a single line, i.e., do not break long queries to more lines.
 SELECT  co.*,c.*,d.dept,d.description, d.totalECTS, t.* FROM course_offers co, courses c,degrees d ,teacher_assignments ta, teachers t WHERE co.offerID = 1 AND co.cID = c.cID AND c.dID = d.dID AND (ta.offerID = co.offerID and ta.tID = t.tID );
 SELECT co.*, st.*, d.* FROM student_registrations sr, course_offers co, students st, degrees d, student_assistants sa WHERE sr.rID=3 AND sa.rID=sr.rID AND sa.offerID=co.offerID AND sr.sID=st.sID AND d.dID=sr.dID;
-SELECT average(grade) FROM course_registrations cr WHERE cr.rID = 3;
+SELECT AVG(grade) FROM course_registrations cr WHERE cr.rID = 3;
