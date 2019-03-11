37bb73af6248d5756c9c7b2b3de9a8990e5f00c7
Adrian Cucos <dielorg@gmail.com>
2019-03-11 16:26:18 +0100

    intial queries

--- q2Queries.sql
+++ q2Queries.sql
@@ -3,11 +3,12 @@
 -- Do not break long queries to more than one line, and do not leave empty lines between them. 
 -- If you do not know the answer to one of the sub-questions, then write SELECT 0;
 -- Example: Let's say I only know sub-question 3:
+SELECT c.name, cr.grade FROM courses c, student_registrations sr, course_registrations cr, course_offers co WHERE sr.sID = %1% AND sr.dID = %2% AND sr.rID = cr.rID AND cr.offerID = co.offerID AND co.cID = c.cID;
+SELECT 0;
+SELECT 0; 
 SELECT 0;
 SELECT 0;
-SELECT phonenumber from myphonebook where name=%1%; 
 SELECT 0;
 SELECT 0;
-SELECT 0;
-SELECT 0;
+SELECT 0; -- extra for now
 WITH required_assistants (offerID, count) AS (SELECT cr.offerID, 1+COUNT(cr.rID)/50 FROM course_registrations cr GROUP BY cr.offerID), actual_assistants(offerID, count) AS (SELECT offerID, COUNT(sa.rID) FROM student_assistants sa GROUP BY sa.offerID) SELECT c.name, co.year, co.quartile, co.offerID, aa.nr as actual_count, ra.count as required_count FROM required_assistants ra, actual_assistants aa, course_offers co, courses c WHERE co.offerID = ra.offerID AND co.cID = c.cID AND aa.offerID = co.offerID AND aa.count < ra.count ORDER BY co.offerId;
